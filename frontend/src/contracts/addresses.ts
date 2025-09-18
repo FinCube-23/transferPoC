2be264a981fc732c6b400b9c7aa938f5375ca1e8
omitul.islam <omitul.islam@brainstation-23.com>
2025-09-18 17:42:15 +0600

    fix: fixed the issue, env not loaded

--- frontend/src/contracts/addresses.ts
+++ frontend/src/contracts/addresses.ts
@@ -1,13 +1,13 @@
 export interface ContractAddresses {
     finCube: string;
     finCubeDAO: string;
-    fincubeERC20: string; // renamed properly
+    fincubeERC20: string;
     mockERC20: string;
 }
 
 export const contractAddresses = {
-    finCube: import.meta.env.VITE_FINCUBE_CONTRACT,
-    finCubeDAO: import.meta.env.VITE_FINCUBE_DAO_CONTRACT,
-    mockERC20: import.meta.env.VITE_MOCK_ERC20_CONTRACT,
+    finCube: import.meta.env.VITE_FINCUBE_CONTRACT!,
+    finCubeDAO: import.meta.env.VITE_FINCUBE_DAO_CONTRACT!,
+    mockERC20: import.meta.env.VITE_MOCK_ERC20_CONTRACT!,
 };
 
